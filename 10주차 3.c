16fbe47e61bf25c056a82bd2c3ac73a09e0ede83
kingchan123 <rlaalscks05@gmail.com>
2024-11-10 17:18:32 +0900

    Update 10주차 3.c

--- 10주차 3.c
+++ 10주차 3.c
@@ -1,34 +1,54 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-int getline(char* line) {
-    int ch;
-    int i = 0;
-    while((ch = getchar()) != '\n') line[i++] = ch;
-    line[i] = '\0';
-    return i;
+typedef struct point {
+    int x;
+    int y;
+} POINT;
+
+POINT makePoint(void) {
+    POINT p;
+    printf("Enter x and y: ");
+    scanf("%d %d", &p.x, &p.y);
+    return p;
 }
 
+void makeRandomPoints(POINT p[], int size) {
+    srand((unsigned int)time(NULL));
+    for (int i = 0; i < size; i++) {
+        p[i].x = rand() % 100;
+        p[i].y = rand() % 100;
+    }
+}
+
+void printPoints(POINT p[], int size) {
+    for (int i = 0; i < size; i++) {
+        printf("point[%d] = x: %d, y: %d\n", i, p[i].x, p[i].y);
+    }
+}
+void saveFile(POINT p[], int size){
+    FILE* fp;
+    fp = fopen("struct.bin", "wb");
+    if(fp == NULL) exit(-1);
+    fwrite(p, sizeof(POINT), size, fp);
+    fclose(fp);
+    
+}
+void loadFile(POINT p[], int size){
+    FILE* fp;
+    fp = fopen("struct.bin", "rb");
+    if(fp == NULL) exit(-1);
+    fread(p, sizeof(POINT), size, fp);
+    fclose(fp);
+}
 int main() {
-    char input[100];
-    FILE* fp_src;
-    FILE* fp_dest;
-
-    if((fp_src = fopen("output.txt", "r")) == NULL) {
-        printf("error...");
-        return 0;
-    }
-    if((fp_dest = fopen("output2.txt", "w")) == NULL) {
-        printf("error...");
-        return 0;
-    }
-
-    while (!feof(fp_src)) {
-        fgets(input, 100, fp_src);
-        fputs(input, fp_dest);
-    }
-
-    fclose(fp_src);
-    fclose(fp_dest);
+    POINT p[10];
+    //makeRandomPoints(p, 10);
+    loadFile(p,10);
+    printPoints(p, 10);
+    //saveFile(p,10);
+    
 
     return 0;
 }
